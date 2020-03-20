f7e4a3c25a315a506f6e3e15d247c376e214d85d
Toqi Tahamid <toqitahmid@gmail.com>
2020-03-20 18:42:12 +0600

    added ant.design

---
+++ src/components/CountryStats.js
@@ -0,0 +1,59 @@
+import useStats from "../utils/useStats";
+//import styled from "styled-components";
+import React from "react";
+//import GetActiveStats from "./GetActiveStats";
+import {Card, Col, Row} from 'antd';
+
+//const style = {background: '#fff', padding: '8px 0'};
+
+function CountryStats({url}) {
+
+    const {stats, loading, error} = useStats(url);
+
+
+    //console.log(error);
+    //if (!stats) return <p>Loading...</p>
+    if (loading) return <p>Loading...</p>;
+    if (error || !stats) return <p>Error</p>;
+    const active = stats.confirmed.value - stats.deaths.value - stats.recovered.value;
+
+    return (
+        <div>
+
+            <Row gutter={{xs: 8, sm: 16, md: 24, lg: 32}}>
+
+                <Col span={6}>
+                    <Card headStyle={{background: '#f0f2f5'}} title="Confirmed">
+                        <span>{stats.confirmed.value}</span>
+                    </Card>
+                </Col>
+
+                <Col span={6}>
+                    <Card headStyle={{background: '#f0f2f5'}} title="Deaths">
+                        <span>{stats.deaths.value}</span>
+                    </Card>
+                </Col>
+
+
+                <Col span={6}>
+                    <Card headStyle={{background: '#f0f2f5'}} title="Active">
+                        <span>
+                            {/*<GetActiveStats url={`${url}/confirmed`}/>*/}
+                            {active}
+                        </span>
+                    </Card>
+                </Col>
+
+                <Col span={6}>
+                    <Card headStyle={{background: '#f0f2f5'}} title="Recovered">
+
+                        <span>{stats.recovered.value}</span>
+                    </Card>
+                </Col>
+            </Row>
+
+        </div>
+    );
+}
+
+export default CountryStats;
